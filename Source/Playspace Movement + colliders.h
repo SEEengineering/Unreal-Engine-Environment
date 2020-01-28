d927a813f760a26b655cb970025ccfaaf032ad39
SEEengineering <57258143+SEEengineering@users.noreply.github.com>
2020-01-28 21:42:50 +0000

    Create Playspace Movement + colliders.h

---
+++ Source/Playspace Movement + colliders.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerController.h"
+#include "VRCharacter.generated.h"
+
+UCLASS()
+class ARCHITECTUREXPLORER_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	
+  //float for continouos values
+	void MoveForward(float throttle);
+	void MoveRight(float throttle);
+
+
+//private section to store refence to my camera
+private:
+	
+  //camera and componenent will show up with UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere)
+	class USceneComponent* VRRoot;
+
+
+	
+};
